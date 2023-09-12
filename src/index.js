6d370fba40c51f29633b30541f5016ef704b0199
Старовойтова Елизавета <starovojtovaelizaveta@MacBook-Air-Starovojtova.local>
2023-09-12 23:04:57 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    // Convert the number to a string and remove the negative sign if it exists
+    let numStr = Math.abs(n).toString();
   
-}
+    let numArr = numStr.split('');
+  
+    let reversedArr = numArr.reverse();
+  
+    let reversedStr = reversedArr.join('');
+  
+    // Convert the reversed string back to a number and add back the negative sign if needed
+    let resultNumber = parseInt(reversedStr);
+    if (n < 0) {
+      resultNumber *= 1;
+    }
+  
+    return resultNumber;
+  }
+
+  console.log(module.exports(-251));
+  
